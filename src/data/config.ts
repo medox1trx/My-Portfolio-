a8071517a08d4e878c8d949c36d822ae2a886505
MedoxX <mohamedalitkhsaoui@gmail.com>
2026-01-13 01:02:02 +0100

    a

---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+const config = {
+  title: "Mohamed Ali Takhsaoui| DEV",
+  description: {
+    long: "Explore the portfolio of Mohamedali, Web Full Stack Developer with 2 years of experience building full web applications — from databases to modern user interfaces. Passionate about clean code and turning ideas into efficient solutions.",
+    short:
+      "Discover the portfolio of Mohamedali, a Full stack Dev.",
+  },
+  keywords: [
+    "MohamedAli Takhsaoui",
+    "MohamedAli Takh",
+    "MohamedAlitakhsaoui",
+    "MohamedAli_TRX.com",
+    "MohamedAli Takhsaoui portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    // "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    "React",
+    "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "MohamedAli Takhsaoui",
+  email: "mohamedalitkhsaoui@gmail.com",
+  site: "#",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/Medox_TRX/",
+    linkedin: "https://www.linkedin.com/in/takhsaoui-mohamedali/",
+    instagram: "https://www.instagram.com/_g1___u/",
+    facebook: "https://github.com/medox1trx/",
+    github: "https://github.com/medox1trx/",
+  },
+};
+export { config };
